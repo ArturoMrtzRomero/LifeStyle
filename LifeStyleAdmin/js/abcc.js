5c3fbb436a583dde161a7bab74b73bb1b19748a1
MilagrosMorenoR <milagrosmoreno.r@outlook.com>
2022-05-10 17:38:22 -0500

    abcc
    se agregaron campos para agregar y editar el abcc

---
+++ LifeStyleAdmin/js/abcc.js
@@ -0,0 +1,73 @@
+function agregarProducto() {
+    console.log( "Ejecutando función test()" );
+    document.getElementById('premio').style.display = 'block';
+                                                
+}
+
+function ocultarProductos(){
+    console.log( "Ejecutando()" );
+    document.getElementById('premio').style.display = 'none';
+    var checkbox = document.getElementById("agregarProducto"); // Aqui seleccionas tu checkbox.
+    checkbox.checked=false;
+}
+function editarProducto() {
+    console.log( "Ejecutando función test()" );
+    document.getElementById('editarProducto').style.display = 'block';
+                                                
+}
+function ocultarEditP(){
+    console.log( "Ejecutando()" );
+    document.getElementById('editarProducto').style.display = 'none';
+    var checkbox = document.getElementById("editarProducto"); // Aqui seleccionas tu checkbox.
+    checkbox.checked=false;
+}
+
+// function de usuario
+function agregarUsiario() {
+    console.log( "Ejecutando función test()" );
+    document.getElementById('usuario').style.display = 'block';
+                                                
+}
+
+function ocultarUsuario(){
+    console.log( "Ejecutando()" );
+    document.getElementById('usuario').style.display = 'none';
+    var checkbox = document.getElementById("agregarUsuario"); // Aqui seleccionas tu checkbox.
+    checkbox.checked=false;
+}
+function editarUsuario() {
+    console.log( "Ejecutando función test()" );
+    document.getElementById('editarUsuario').style.display = 'block';
+                                                
+}
+function ocultarEditU(){
+    console.log( "Ejecutando()" );
+    document.getElementById('editarUsuario').style.display = 'none';
+    var checkbox = document.getElementById("editarUsuario"); // Aqui seleccionas tu checkbox.
+    checkbox.checked=false;
+}
+
+// function de orden
+function agregarOrden() {
+    console.log( "Ejecutando función test()" );
+    document.getElementById('orden').style.display = 'block';
+                                                
+}
+
+function ocultarOrden(){
+    console.log( "Ejecutando()" );
+    document.getElementById('orden').style.display = 'none';
+    var checkbox = document.getElementById("agregarOrden"); // Aqui seleccionas tu checkbox.
+    checkbox.checked=false;
+}
+function editarOrden() {
+    console.log( "Ejecutando función test()" );
+    document.getElementById('editarOrden').style.display = 'block';
+                                                
+}
+function ocultarEditO(){
+    console.log( "Ejecutando()" );
+    document.getElementById('editarOrden').style.display = 'none';
+    var checkbox = document.getElementById("editarOrden"); // Aqui seleccionas tu checkbox.
+    checkbox.checked=false;
+}
